49e6964898e21cffd56b738189259b44e8508a24
GavincMcNeil <136871213+GavincMcNeil@users.noreply.github.com>
2023-07-27 22:22:52 -0400

    inital commit and added a header, description of the quiz and quiz button as well as started on a function for the timer

---
+++ script.js
@@ -0,0 +1,8 @@
+var timer = 30
+var elem = document.getElementById('Timer')
+var timerId = setInterval(countdown,1000);
+function countdown() {
+    if (timeLeft === 0) {
+        clearTimeout(timerId)
+    }
+}
